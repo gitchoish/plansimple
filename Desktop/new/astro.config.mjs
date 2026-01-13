f56357f0508dbbfddd67f4f9881807906bd74f3b
gitchoish <151187722+gitchoish@users.noreply.github.com>
2026-01-13 22:27:57 +0900

    Remove all files - preparing clean import

--- Desktop/new/astro.config.mjs
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-
-// https://astro.build/config
-export default defineConfig({
-  integrations: [tailwind()],
-  output: 'static', // Cloudflare Pages 정적 배포용
-});
